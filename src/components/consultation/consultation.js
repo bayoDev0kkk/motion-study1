3650f52cb8036a70ef27d7b9778798d45df5fb4b
bayoDev0kkk <kanybekov211@mail.ru>
2022-10-06 20:16:37 +0600

    scss

--- src/components/consultation/consultation.js
+++ src/components/consultation/consultation.js
@@ -10,7 +10,7 @@ const Consultation = () => {
                     <div className="consultation-general__input">
                         <form action="#">
                             <label htmlFor="#"><input type="text" name='name' placeholder='Name' required /></label>
-                            <label htmlFor="#"><input type="number" name='Phone' placeholder='Phone' required /></label>
+                            <label htmlFor="#"><input type="tel" name='Phone' placeholder='Phone' required /></label>
                             <label htmlFor="#"> <input type="Email" name='Email' placeholder='Email' required /></label>
                         </form>
                         <div className="btn"><button>Send</button></div>
